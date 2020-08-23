36edc0b53cda0d93b7e7057a57423fc3d4ea2baf
Stephen Enke <stephenenke@gmail.com>
2020-08-23 21:28:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 var recipes = {}
- updateObjectWithKeyAndValue(object, key, value) {
-  var object = { key: 'value' };
-  return object
-  var objectNew = object.assign({}, object, {[key]: value})
+function updateObjectWithKeyAndValue(object, key, value) {
+ return object.assign({}, object, {[key]: value})
   return objectNew
 }

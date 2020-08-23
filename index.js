84d2e984087557deb0261faa47c57bfcb43bf119
Stephen Enke <stephenenke@gmail.com>
2020-08-23 21:39:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object
 }
+function deleteFromObjectByKey(object, key) {
+  var obj = { object: 'key' }
+  var newObj = Object.assign({}, obj)
+  delete newObj.key
+  return newObj
+}

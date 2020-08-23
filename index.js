ae346a10cab2f6ee7776218854c88fe978d6360a
Stephen Enke <stephenenke@gmail.com>
2020-08-23 21:48:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ function deleteFromObjectByKey(object, key) {
   return newObj
 }
 function destructivelyDeleteFromObjectByKey(object, key) {
-  var object = { key: '' }
-  delete object.key;
+  delete object[key];
   return object;
 }
